983a5efc1be81a65417d7567d1577b12e663018d
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-28 15:00:12 +0800

    build login screen with logo background

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import {
+	View,
+	Text,
+	Button,
+	ImageBackground,
+	TouchableOpacity
+} from 'react-native';
+
 import tw from 'tailwind-rn';
 import useAuth from '../hooks/useAuth';
 
 const LoginScreen = () => {
 	const { signInWithGoogle } = useAuth();
 	return (
-		<View style={tw('flex-1 justify-center items-center')}>
-			<Text>Login in to the app</Text>
-			<Button title='Log in with Google' onPress={signInWithGoogle} />
+		<View style={tw('flex-1')}>
+			<ImageBackground
+				resizeMode='cover'
+				style={tw('flex-1')}
+				source={require('../assets/logo.png')}
+			>
+				<TouchableOpacity
+					style={[
+						tw('absolute bottom-40 w-52 bg-white p-4 rounded-2xl'),
+						{ marginHorizontal: '25%' }
+					]}
+				>
+					<Text
+						style={tw('font-semibold text-center')}
+						onPress={signInWithGoogle}
+					>
+						Login in with Google
+					</Text>
+				</TouchableOpacity>
+			</ImageBackground>
 		</View>
 	);
 };

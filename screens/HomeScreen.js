7fcfb11978a899224e733360d1c03ebf4ab9398b
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-28 14:44:48 +0800

    pass in logout function in HomeScreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import useAuth from '../hooks/useAuth';
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
+	const { logout } = useAuth();
 	return (
 		<View>
 			<Text>Vocabufy Home</Text>
@@ -11,6 +13,7 @@ const HomeScreen = () => {
 				title='Explore deck'
 				onPress={() => navigation.navigate('Deck')}
 			/>
+			<Button title='Log out' onPress={logout} />
 		</View>
 	);
 };

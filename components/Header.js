3858f26cd0e84381c65a983de702fa81bc91a221
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-28 15:32:56 +0800

    integrate Header in HomeScreen and restyle Header and HomeScreen

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { View, TouchableOpacity, Image } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import tw from 'tailwind-rn';
 import { FontAwesome5 } from '@expo/vector-icons';
 
 const Header = (props) => {
 	const { user, navigation } = props;
 	return (
-		<View style={tw('flex-row items-center justift-between px-5')}>
+		<View
+			style={tw('flex-row items-center justify-between px-5 mt-3 mb-5')}
+		>
 			<TouchableOpacity>
 				<Image
-					style={tw('h-10 w-10 rounded-full')}
+					style={tw('h-8 w-8 rounded-full')}
 					source={{ uri: user.photoURL }}
 				/>
 			</TouchableOpacity>
 			<TouchableOpacity>
-				<Image
-					style={tw('h-14 w-14')}
-					source={require('../assets/header.png')}
-				/>
+				<Text style={tw('text-gray-800 text-xl font-medium')}>
+					Vocabufy
+				</Text>
 			</TouchableOpacity>
 			<TouchableOpacity onPress={() => navigation.navigate('Deck')}>
-				<FontAwesome5 name='book' size={24} color='black' />
+				<FontAwesome5 name='book' size={28} color='black' />
 			</TouchableOpacity>
 		</View>
 	);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,25 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { Text, Button, SafeAreaView } from 'react-native';
 import useAuth from '../hooks/useAuth';
+import tw from 'tailwind-rn';
+import Header from '../components/Header';
 
 const HomeScreen = () => {
 	const navigation = useNavigation();
-	const { logout } = useAuth();
+	const { logout, user } = useAuth();
 	return (
-		<View>
+		<SafeAreaView style={tw('flex-1')}>
+			{/* Header */}
+			<Header navigation={navigation} user={user} />
+			{/* Swiper */}
 			<Text>Vocabufy Home</Text>
 			<Button
 				title='Explore deck'
 				onPress={() => navigation.navigate('Deck')}
 			/>
 			<Button title='Log out' onPress={logout} />
-		</View>
+		</SafeAreaView>
 	);
 };
 

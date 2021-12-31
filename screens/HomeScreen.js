b6252c10fda5e35922f2c88f7713199731f85c3f
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-31 14:25:42 +0800

    add useLayoutEffect to open modal on first login

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import { Button, SafeAreaView } from 'react-native';
 import useAuth from '../hooks/useAuth';
 import tw from 'tailwind-rn';
 import Header from '../components/Header';
 import Cards from '../components/Cards';
+import { doc, onSnapshot } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
 	const { logout, user } = useAuth();
+	const navigation = useNavigation();
+
+	useLayoutEffect(
+		() =>
+			onSnapshot(doc(db, 'users', user.uid), (snapshot) => {
+				if (!snapshot.exists()) {
+					navigation.navigate('Info');
+				}
+			}),
+		[]
+	);
+
 	return (
 		<SafeAreaView style={tw('flex-1')}>
 			{/* Header */}

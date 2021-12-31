c587c63909fc72ad1b44eb4be3cf1e0542e90909
Ka Wing Tsang <kawingt2@gmail.com>
2021-12-31 14:35:45 +0800

    add default no card UI

--- components/Cards.js
+++ components/Cards.js
@@ -1,27 +1,28 @@
 import React, { useRef } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import Swiper from 'react-native-deck-swiper';
 import tw from 'tailwind-rn';
 import CardButtons from './CardButtons';
 
 const Cards = () => {
 	let MOCK_DATA = [
-		{
-			word: 'essential',
-			pos: 'adjective',
-			id: '123'
-		},
-		{
-			word: 'assume',
-			pos: 'verb',
-			id: '234'
-		},
-		{
-			word: 'engineer',
-			pos: 'noun',
-			id: '335'
-		}
+		// {
+		// 	word: 'essential',
+		// 	pos: 'adjective',
+		// 	id: '123'
+		// },
+		// {
+		// 	word: 'assume',
+		// 	pos: 'verb',
+		// 	id: '234'
+		// },
+		// {
+		// 	word: 'engineer',
+		// 	pos: 'noun',
+		// 	id: '335'
+		// }
 	];
+
 	const swipeRef = useRef(null);
 	return (
 		<>
@@ -59,28 +60,64 @@ const Cards = () => {
 							}
 						}
 					}}
-					renderCard={(card) => (
-						<View
-							key={card.id}
-							style={[
-								tw(
-									'bg-white h-3/4 rounded-xl flex-col items-center justify-center'
-								),
-								styles.cardShadow
-							]}
-						>
-							<Text
-								style={tw(
-									'text-gray-800 text-xl text-xl items-center'
-								)}
+					renderCard={(card) =>
+						card ? (
+							<View
+								key={card.id}
+								style={[
+									tw(
+										'bg-white h-3/4 rounded-xl flex-col items-center justify-center'
+									),
+									styles.cardShadow
+								]}
 							>
-								{card.word}
-							</Text>
-							<Text style={tw('text-gray-600 text-xl text-xl')}>
-								{card.pos}
-							</Text>
-						</View>
-					)}
+								<Text
+									style={tw(
+										'text-gray-800 text-xl text-xl items-center'
+									)}
+								>
+									{card.word}
+								</Text>
+								<Text
+									style={tw('text-gray-600 text-xl text-xl')}
+								>
+									{card.pos}
+								</Text>
+							</View>
+						) : (
+							<View
+								style={[
+									tw(
+										'bg-white h-3/4 rounded-xl flex-col items-center justify-center'
+									),
+									styles.cardShadow
+								]}
+							>
+								<Text
+									style={tw(
+										'text-gray-800 text-xl items-center pb-5'
+									)}
+								>
+									Sad (adj.)
+								</Text>
+								<Text
+									style={tw(
+										'text-gray-800 text-xl items-center'
+									)}
+								>
+									傷心--因為沒有新詞彙
+								</Text>
+								<Image
+									style={tw('h-20 w-full')}
+									height={100}
+									width={100}
+									source={{
+										uri: 'https://links.papareact.com/6gb'
+									}}
+								/>
+							</View>
+						)
+					}
 				/>
 			</View>
 			<CardButtons swipeRef={swipeRef} />

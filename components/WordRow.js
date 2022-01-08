ee9e8a3ba2762b383f8288ebb1436d9b8acc0871
Ka Wing Tsang <kawingt2@gmail.com>
2022-01-08 18:26:52 +0800

    remove sound icons

--- components/WordRow.js
+++ components/WordRow.js
@@ -9,26 +9,23 @@ const WordRow = ({ wordDetails }) => {
 		<TouchableOpacity
 			style={[
 				tw(
-					'flex-row items-center justify-between py-3 px-5 bg-white mx-3 my-1 rounded-lg'
+					'flex-row items-center py-3 px-5 bg-white mx-3 my-1 rounded-lg'
 				),
 				styles.cardShadow
 			]}
 		>
-			<View style={[tw('rounded-full h-8 w-25 mr-4 items-center')]}>
+			<View style={[tw('rounded-full h-8 w-33 mr-4 items-center')]}>
 				{wordDetails.tag === 'known' ? (
 					<Entypo name='check' size={24} color='green' />
 				) : (
 					<Entypo name='cross' size={24} color='red' />
 				)}
 			</View>
-			<View style={tw('w-50')}>
+			<View style={tw('ml-5')}>
 				<Text style={tw('text-lg font-semibold')}>
 					{wordDetails.word} ({wordDetails.pos})
 				</Text>
 				<Text style={tw('text-lg mt-3')}>{wordDetails.meaning}</Text>
-			</View>
-			<View style={tw('w-25')}>
-				<AntDesign name='sound' size={24} color='black' />
 			</View>
 		</TouchableOpacity>
 	);
